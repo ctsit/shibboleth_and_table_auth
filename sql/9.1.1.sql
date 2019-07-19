476b78542baf15699b9a140e8a5778bd63cb1385
Philip Chase <pbc@ufl.edu>
2019-07-19 11:22:50 -0400

    Replace sql patch with actual sql file

---
+++ sql/9.1.1.sql
@@ -0,0 +1,5 @@
+REPLACE INTO redcap_config(field_name,value) VALUES('shibboleth_table_login_image', 'https://wiki.shibboleth.net/confluence/download/attachments/131074/atl.site.logo?version=2&modificationDate=1502412080059&api=v2');
+REPLACE INTO redcap_config(field_name,value) VALUES('shibboleth_table_shibboleth_login_option', 'Use Shibboleth');
+REPLACE INTO redcap_config(field_name,value) VALUES('shibboleth_table_table_login_option', 'Use Table');
+REPLACE INTO redcap_config(field_name,value) VALUES('shibboleth_table_shibboleth_login_text', 'Descriptive Shibboleth text');
+REPLACE INTO redcap_config(field_name,value) VALUES('shibboleth_table_splash_default', 'shib');
