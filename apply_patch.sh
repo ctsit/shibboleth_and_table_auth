1a45a11b838b4de91efbb9d4f6c657ea7bdc4ba0
Philip Chase <pbc@ufl.edu>
2019-07-19 13:28:32 -0400

    Update apply_patch.sh
    Specify input file name on command line.
    Support patching of Upgrade zips.
    Extract REDCAP_VERSION from input filename.
    Write output to a separate output folder.
    Make temp folder with mktemp.
    Unzip, zip, and pushd quietly.
    Echo progress messages.
    Ask users to create issues when they see errors.

--- apply_patch.sh
+++ apply_patch.sh
@@ -1,41 +1,54 @@
 #!/bin/bash
 
-#TODO: read from file? Process as args?
-REDCAP_VERSION=9.1.1
+#TODO: test for required arg
+if [ ! -e ${1} ]; then
+    echo "Please provide the filename of redcap zip file you wish to patch."
+    exit
+fi
+
+REDCAP_ZIP=${1}
 REDCAP_PATCH_VERSION=9.1.1
+REDCAP_VERSION=`echo ${REDCAP_ZIP} | sed "s/redcap//; s/_upgrade//; s/.zip//;"`
 
 # determine the directory where this script resides
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# create a random 15 char alphanumeric string to avoid any possible clashes
-RAND="$(head -n 1 /dev/urandom | tr -dc '[:alnum:]' | fold -w 15 | head -n 1)"
+# make sure we have a place to write our output
+TARGET_DIR=${DIR}/output
+if [[ ! -e  $TARGET_DIR ]]; then
+    mkdir ${TARGET_DIR}
+fi
+TARGET_FILE=output/${REDCAP_ZIP}
 
-TEMP_UNZIP_DIR=/tmp/redcap_unzip_${RAND}
+# unzip files into a temporary redcap_unzip directory
+echo "Unzipping ${REDCAP_ZIP} into a temporary folder"
+TEMP_UNZIP_DIR=`mktemp -d`
+unzip -q ${REDCAP_ZIP} -d $TEMP_UNZIP_DIR
 
-# unzip files into redcap_unzip directory
-unzip redcap${REDCAP_VERSION}.zip -d $TEMP_UNZIP_DIR
-
-pushd $TEMP_UNZIP_DIR/redcap/redcap_v${REDCAP_VERSION}
+pushd $TEMP_UNZIP_DIR/redcap/redcap_v${REDCAP_VERSION} > /dev/null
     DRY="$(patch -p4 --dry-run < $DIR/redcap-${REDCAP_PATCH_VERSION}.patch)"
     # first check if the patch has any issues
     if [[ $DRY == *"FAILED"* ]]; then
         echo "Patching has failed due to "
         echo $DRY
+        echo "Please report this error as an issue at https://github.com/ctsit/shibboleth_and_table_auth/issues"
+        echo "Please include the text of the error and the REDCap version you were trying to patch."
         exit 1
     elif [[ $DRY == *"offset"* ]]; then
         echo "Patching was offset but will proceed"
         echo $DRY
+        echo "Please report this warning as an issue at https://github.com/ctsit/shibboleth_and_table_auth/issues"
+        echo "Please include the text of the warning and the REDCap version you are patching."
     fi
-    echo "REDCap ${REDCAP_VERSION} was successfully patched"
     patch -p4 < $DIR/redcap-${REDCAP_PATCH_VERSION}.patch # actually perform the patch
+    echo "REDCap ${REDCAP_ZIP} was successfully patched"
 
-    patch -p4 < $DIR/redcap-sql-${REDCAP_PATCH_VERSION}.patch # adds the SQL file needed for customization
+echo "Zipping patched redcap into ${TARGET_FILE}"
+pushd $TEMP_UNZIP_DIR > /dev/null
+    zip -q -ur ${TARGET_DIR}/${REDCAP_ZIP} . # write a new zip file with the patch applied
 
-pushd $TEMP_UNZIP_DIR
-    zip -ur $DIR/redcap${REDCAP_VERSION}.zip . # update the zip file with the new information
-
-pushd -0 && dirs -c # return to home directory and clear directory stack
+pushd -0 > /dev/null && dirs -c # return to home directory and clear directory stack
 
 rm -rf $TEMP_UNZIP_DIR
 
-echo "Please use the patched zip file, redcap${REDCAP_VERSION}, as you normally would."
+echo "Please use the patched zip file, ${TARGET_FILE}, as you normally would."
