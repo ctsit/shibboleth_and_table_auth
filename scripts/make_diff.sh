ef59c59f3296568aaa60c4690089e78651ec1984
Kyle Chesney <kyle.2493@gmail.com>
2019-08-20 16:53:20 -0400

    add version config, use it for REDCap repo path

--- scripts/make_diff.sh
+++ scripts/make_diff.sh
@@ -5,10 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $DIR/version.config
 
-REDCAP_DIR=~/projects/REDCap
-
-pushd $REDCAP_DIR
-git diff -U5 -p -r --diff-algorithm=minimal master..shibboleth-table-auth --output=$DIR/../redcap-${REDCAP_VERSION}.patch
+pushd $VANDY_REDCAP_DIR
+git diff -U15 -p -r --diff-algorithm=minimal master..shibboleth-table-auth --output=$DIR/../redcap-${REDCAP_VERSION}.patch
 popd
 
 # replace all non-diff lines with CRLF line endings
